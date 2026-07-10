4ccd2c81ce4232faa0a1c0d9a8d637497be299c5
visualtailstudios <christiangrice0@gmail.com>
2026-07-10 02:28:48 -0500

    updated values, css, ect

--- pricecalculator.py
+++ pricecalculator.py
@@ -1,2 +1,17 @@
-print((20*2)*23)
+
+#print((20*2)*23)
 # print((14*10)+200)
+
+lesson = 10
+time = 14
+
+result = (time * 2) * lesson
+
+final = 0
+
+if result > 200:
+    final = result - 80
+else:
+    final = result
+
+print(final)
